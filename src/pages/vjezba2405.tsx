85efa289e206b95fdf4d26b71878f0eba4a8dc3e
grafika10 <grafika10@ucione.local>
2023-05-26 21:55:42 +0200

    add favorites, uredna stranica

--- src/pages/vjezba2405.tsx
+++ src/pages/vjezba2405.tsx
@@ -1,4 +1,3 @@
-import logo from "./../assets/Logo.png";
 import { ListOfPlayers, PlayersType } from "../data/Listofplayers";
 import ClubInfo from "../components/ClubInfo";
 
@@ -6,6 +5,7 @@ import { useState } from "react";
 const Vjezba2405 = () => {
   const [activeModal, setActiveModal] = useState<boolean>(false);
   const [activePlayer, setActivePlayer] = useState<PlayersType | null>(null);
+  const [favorites, setFavorites] = useState<string[]>([]);
   const findPlayer = (number: number) => {
     ListOfPlayers.forEach((player) => {
       if (number === player.shirtNumber) {
@@ -13,72 +13,89 @@ const Vjezba2405 = () => {
       }
     });
   };
+  const addToFavorites = (item: string) => {
+    setFavorites((prevFavorites) => [...prevFavorites, item]);
+  };
   return (
-    <span>
+    <span className="Vjezba2405__container">
       <header className="Vjezba2405__header">
-        <img
-          className="Vjezba2405__header__logo"
-          src={logo}
-          alt="Logo"
-          height={200}
-          width={200}
-        />
+        <h1>MAN UTD</h1>
       </header>
-      <body className="Vjezba2405__body">
+      <div className="Vjezba2405__body">
         <ClubInfo />
-        <hr />
+      </div>
+      <div className="Vjezba2405__body">
         <h2>
           List of all promenant 1st squad active players of the 22/23 season
         </h2>
-        <hr />
-        {ListOfPlayers.map((link) => {
-          return (
-            <div
-              onClick={() => {
-                findPlayer(link.shirtNumber);
-                setActiveModal(true);
-              }}
-            >
-              {link.name}
-            </div>
-          );
-        })}
-        {activeModal ? (
-          <div className={`modal ${activeModal ? "isactive" : ""}`}>
-            <div>
-              <a
-                title="Close"
-                className="close"
+
+        <div>
+          {ListOfPlayers.map((link) => {
+            return (
+              <button
+                className="button"
                 onClick={() => {
-                  setActiveModal(false);
+                  findPlayer(link.shirtNumber);
+                  setActiveModal(true);
+                  addToFavorites(link.name);
                 }}
               >
-                X
-              </a>
-              <div>
-                {activePlayer ? (
+                {link.name}
+              </button>
+            );
+          })}
+          <div>
+            {activeModal ? (
+              <div className={`modal ${activeModal ? "isactive" : ""}`}>
+                <div>
+                  <a
+                    title="Close"
+                    className="close"
+                    onClick={() => {
+                      setActiveModal(false);
+                    }}
+                  >
+                    X
+                  </a>
                   <div>
-                    <img
-                      src={activePlayer.picture}
-                      alt=""
-                      height={100}
-                      width={100}
-                      className="Vjezba2405__img"
-                    />
-                    <div>Name:{activePlayer.name}</div>
-                    <div>Shirt Number: {activePlayer.shirtNumber}</div>
-                    <div>Position: {activePlayer.position}</div>
+                    {activePlayer ? (
+                      <div className="card">
+                        <img
+                          src={activePlayer.picture}
+                          alt=""
+                          className="card__img"
+                          height={50}
+                          width={50}
+                        />
+                        <hr />
+                        <div className="card__title">{activePlayer.name}</div>
+                        <div className="card__desc">
+                          {activePlayer.shirtNumber}
+                        </div>
+                        <div className="card__desc">
+                          {activePlayer.position}
+                        </div>
+                      </div>
+                    ) : (
+                      <div>Niti jedan Player nije odabran.</div>
+                    )}
                   </div>
-                ) : (
-                  <div>Niti jedan Player nije odabran.</div>
-                )}
+                </div>
               </div>
-            </div>
+            ) : (
+              ""
+            )}
           </div>
-        ) : (
-          ""
-        )}
-      </body>
+        </div>
+      </div>
+      <div className="Vjezba2405__body">
+        <h2>
+          Favorites:
+          {favorites.map((favorites, player) => (
+            <div key={player}>{favorites}</div>
+          ))}
+        </h2>
+      </div>
     </span>
   );
 };
